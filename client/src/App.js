8ee1fcbd36b463b18032266d155cbb0d06da8cd8
ryanf27 <ryanfebry0227@gmail.com>
2023-09-30 15:09:53 +0700

    add register page

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home/Home';
-import Order from './pages/Order/Order'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Order from "./pages/Order/Order";
+import Register from "./pages/Register/Register";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />}></Route>
-        <Route path='/order' element={<Order />}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/order" element={<Order />}></Route>
+        <Route path="/register" element={<Register />}></Route>
       </Routes>
     </BrowserRouter>
   );
