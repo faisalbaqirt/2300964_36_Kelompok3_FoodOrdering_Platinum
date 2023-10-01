0af7f48114a82ef66dce074d218ca464e4b3d9bc
faisalbaqirt <faishalb0@gmail.com>
2023-10-01 19:06:59 +0700

    edit and update styles

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,11 +1,20 @@
+import { useEffect } from "react";
 import Navbar from "../../components/Navbar";
 import Header from "../../components/Header";
 import Products from "../../components/Products";
 import Contact from "../../components/Contact";
 import Footer from "../../components/Footer";
+import Scrollspy from "../../utils/Scrollspy"
 import "./home.css";
 
 function Home() {
+  useEffect(() => {
+    const scrollspy = Scrollspy();
+    return () => {
+      scrollspy.dispose();
+    }
+  }, []);
+
   return (
     <div>
       <Navbar />
--- client/src/pages/Order/Order.jsx
+++ client/src/pages/Order/Order.jsx
@@ -1,11 +1,21 @@
+import { useEffect } from "react";
 import Navbar from "../../components/Navbar";
 import Header from "../../components/Header";
 import FormOrder from "../../components/FormOrder";
 import Invoice from "../../components/Invoice"
 import Footer from "../../components/Footer";
+import Scrollspy from "../../utils/Scrollspy";
 import './order.css'
 
 function Order () {
+
+    useEffect(() => {
+        const scrollspy = Scrollspy();
+        return () => {
+          scrollspy.dispose();
+        }
+      }, []);
+
     return(
         <div>
             <Navbar />
