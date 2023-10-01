7cdcc068949c55701a0443b748932642f4fe6cb1
faisalbaqirt <faishalb0@gmail.com>
2023-10-01 19:05:28 +0700

    add function scrollspy

---
+++ client/src/utils/Scrollspy.js
@@ -0,0 +1,10 @@
+const Scrollspy = () => {
+    const scrollspy = new window.bootstrap.ScrollSpy(document.body, {
+      target: "#navigation",
+    });
+  
+    return scrollspy;
+  };
+  
+  export default Scrollspy;
+  
