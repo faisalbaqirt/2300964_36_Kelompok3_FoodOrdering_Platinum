8a225fd2c2123d88ecbe7915c9cc0d994d49950e
faisalbaqirt <faishalb0@gmail.com>
2023-10-04 16:46:02 +0700

    edit component FormOrder

--- client/src/components/FormOrder.jsx
+++ client/src/components/FormOrder.jsx
@@ -1,63 +1,113 @@
-function FormOrder() {
-    return (
-        <div id="order" className="container">
-            <div className="section-title text-center">
-                <h2>Form Pemesanan Produk</h2>
-            </div>
-            <div className="order-content">
-                <form id="order-form">
-                    <label for="product_name">Nama Produk:</label>
-                    <select
-                        id="product_name"
-                        className="form-control"
-                        name="product_name"
-                        required
-                    >
-                        <option value="paket ayam geprek">Paket Ayam Geprek</option>
-                        <option value="ayam geprek">Ayam Geprek</option></select
-                    ><br />
+import { useState } from "react";
+import { createOrder } from "../utils/orderAPI";
 
-                    <label for="quantity">Jumlah:</label>
-                    <input
-                        type="number"
-                        id="quantity"
-                        className="form-control"
-                        name="quantity"
-                        required
-                    /><br />
+const FormOrder = ({ onOrderSubmit }) => {
+  const [product_name, setProductName] = useState("paket ayam geprek");
+  const [quantity, setQuantity] = useState("");
+  const [name, setName] = useState("");
+  const [telephone, setTelephone] = useState("");
+  const [address, setAddress] = useState("");
 
-                    <label for="name">Nama Pemesan:</label>
-                    <input
-                        type="text"
-                        id="name"
-                        className="form-control"
-                        name="name"
-                        required
-                    /><br />
+  const handleOrderSubmit = async (e) => {
+    e.preventDefault();
 
-                    <label for="telephone">Telepon:</label>
-                    <input
-                        type="text"
-                        id="telephone"
-                        className="form-control"
-                        name="telephone"
-                        required
-                    /><br />
+    try {
+      const orderData = {
+        product_name,
+        quantity,
+        name,
+        telephone,
+        address,
+      };
 
-                    <label for="address">Alamat:</label>
-                    <input
-                        type="text"
-                        id="address"
-                        className="form-control"
-                        name="address"
-                        required
-                    /><br />
+      const response = await createOrder(orderData);
 
-                    <button type="submit" id="create-order" className="btn-order">Pesan</button>
-                </form>
-            </div>
-        </div>
-    )
+      if (response && response.order_id) {
+        onOrderSubmit(response.order_id);
+      } else {
+        console.error("Tidak dapat membuat pesanan.");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  return (
+    <div id="order" className="container">
+      <div className="section-title text-center">
+        <h2>Form Pemesanan Produk</h2>
+      </div>
+      <div className="order-content">
+        <form onSubmit={handleOrderSubmit}>
+          <label for="product_name">Nama Produk:</label>
+          <select
+            id="product_name"
+            className="form-control"
+            name="product_name"
+            required
+            value={product_name}
+            onChange={(e) => setProductName(e.target.value)}
+          >
+            <option value="paket ayam geprek">Paket Ayam Geprek</option>
+            <option value="ayam geprek">Ayam Geprek</option>
+          </select>
+          <br />
+
+          <label for="quantity">Jumlah:</label>
+          <input
+            type="number"
+            id="quantity"
+            className="form-control"
+            name="quantity"
+            required
+            value={quantity}
+            onChange={(e) => setQuantity(e.target.value)}
+          />
+          <br />
+
+          <label for="name">Nama Pemesan:</label>
+          <input
+            type="text"
+            id="name"
+            className="form-control"
+            name="name"
+            required
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <br />
+
+          <label for="telephone">Telepon:</label>
+          <input
+            type="text"
+            id="telephone"
+            className="form-control"
+            name="telephone"
+            required
+            value={telephone}
+            onChange={(e) => setTelephone(e.target.value)}
+          />
+          <br />
+
+          <label for="address">Alamat:</label>
+          <input
+            type="text"
+            id="address"
+            className="form-control"
+            name="address"
+            required
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+          />
+          <br />
+
+          <button type="submit" id="create-order" className="btn-order">
+            Pesan
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default FormOrder;
