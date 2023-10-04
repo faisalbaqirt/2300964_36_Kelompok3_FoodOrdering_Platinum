0257c87c2543d7f38e3b341995d97ef43830794a
faisalbaqirt <faishalb0@gmail.com>
2023-10-04 16:49:03 +0700

    edit pages order

--- client/src/pages/Order/Order.jsx
+++ client/src/pages/Order/Order.jsx
@@ -1,30 +1,44 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "../../components/Navbar";
 import Header from "../../components/Header";
 import FormOrder from "../../components/FormOrder";
-import Invoice from "../../components/Invoice"
+import Invoice from "../../components/Invoice";
 import Footer from "../../components/Footer";
 import Scrollspy from "../../utils/Scrollspy";
-import './order.css'
+import "./order.css";
 
-function Order () {
+function Order() {
+  const [isOrderSubmitted, setIsOrderSubmitted] = useState(false);
+  const [orderId, setOrderId] = useState(null);
 
-    useEffect(() => {
-        const scrollspy = Scrollspy();
-        return () => {
-          scrollspy.dispose();
-        }
-      }, []);
+  useEffect(() => {
+    const scrollspy = Scrollspy();
+    return () => {
+      scrollspy.dispose();
+    };
+  }, []);
 
-    return(
-        <div>
-            <Navbar />
-            <Header />
-            <FormOrder />
-            <Invoice />
-            <Footer />
-        </div>
-    )
-};
+  const handleOrderSubmit = (order_id) => {
+    setOrderId(order_id);
+    setIsOrderSubmitted(true);
+  };
+
+  const handleBackToOrder = () => {
+    setIsOrderSubmitted(false);
+  };
+
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      {isOrderSubmitted ? (
+        <Invoice orderId={orderId} onBackToOrder={handleBackToOrder} />
+      ) : (
+        <FormOrder onOrderSubmit={handleOrderSubmit} />
+      )}
+      <Footer />
+    </div>
+  );
+}
 
 export default Order;
