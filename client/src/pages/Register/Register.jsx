83b3bf2d1bdc168111589928d6dc5ee99d959d86
ryanf27 <ryanfebry0227@gmail.com>
2023-10-10 21:16:29 +0700

    add authentication feature

--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -2,53 +2,101 @@ import Contact from "../../components/Contact";
 import Footer from "../../components/Footer";
 import Navbar from "../../components/Navbar";
 import Scrollspy from "../../utils/Scrollspy";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./register.css";
+import axios from "axios";
 
 function Register() {
+  const [formData, setFormData] = useState({
+    email: "",
+    username: "",
+    password: "",
+  });
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleRegistration = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/api/auth/register",
+        formData
+      );
+      if (response.status === 201) {
+        navigate("/login");
+      } else {
+        console.error(response.data.message);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   useEffect(() => {
     const scrollspy = Scrollspy();
     return () => {
       scrollspy.dispose();
     };
   }, []);
+
   return (
     <div className="register">
       <Navbar />
-      <div className="container ">
+      <div className="container">
         <h2 className="text-center">Sign Up</h2>
         <form>
-          <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">
+          <div className="mb-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">
               Email address
             </label>
             <input
               type="email"
-              class="form-control"
+              className="form-control"
               id="exampleInputEmail1"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
               aria-describedby="emailHelp"
             />
-            <div id="emailHelp" class="form-text">
+            <div id="emailHelp" className="form-text">
               We'll never share your email with anyone else.
             </div>
           </div>
-          <div class="mb-3 ">
-            <label for="exampleInputEmail1" class="form-label">
+          <div className="mb-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">
               Username
             </label>
-            <input type="text" class="form-control" />
+            <input
+              type="text"
+              className="form-control"
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
+            />
           </div>
-          <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">
               Password
             </label>
             <input
               type="password"
-              class="form-control"
+              className="form-control"
               id="exampleInputPassword1"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
             />
           </div>
-          <button type="submit" class="btn">
+          <button type="submit" className="btn" onClick={handleRegistration}>
             Sign Up
           </button>
         </form>
--- server/index.js
+++ server/index.js
@@ -1,17 +1,19 @@
-process.env.TZ = 'Asia/Jakarta';
-
-const express = require('express');
-const cors = require('cors')
+process.env.TZ = "Asia/Jakarta";
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
-const productRoutes = require('./routes/ProductRoute');
-app.use('/api/products', productRoutes);
+const productRoutes = require("./routes/ProductRoute");
+app.use("/api/products", productRoutes);
 
-const orderRoutes = require('./routes/OrderRoute');
-app.use('/api/orders', orderRoutes)
+const orderRoutes = require("./routes/OrderRoute");
+app.use("/api/orders", orderRoutes);
+
+const AuthenticationRoutes = require("./routes/AuthenticationRoute");
+app.use("/api/auth", AuthenticationRoutes);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
