c64d59a0d856f9ed875a3af1c5115591b26bf3e6
faisalbaqirt <faishalb0@gmail.com>
2023-10-21 16:46:17 +0700

    update seed for products

--- server/db/seeds/products.js
+++ server/db/seeds/products.js
@@ -6,7 +6,7 @@ exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('products').del()
   await knex('products').insert([
-    { name: 'paket ayam geprek', description: 'ayam geprek beserta nasi', price: 15000 },
-    { name: 'ayam geprek', description: 'ayam geprek', price: 12000 }
+    { name: 'paket ayam geprek', description: 'ayam geprek beserta nasi', price: 15000, image: 'https://res.cloudinary.com/dg1vhnf5g/image/upload/v1697878704/products/ayamgeprek1_n7o1rv.jpg' },
+    { name: 'ayam geprek', description: 'ayam geprek', price: 12000, image: 'https://res.cloudinary.com/dg1vhnf5g/image/upload/v1697878717/products/ayamgeprek2_igudpv.jpg' }
   ]);
 };
