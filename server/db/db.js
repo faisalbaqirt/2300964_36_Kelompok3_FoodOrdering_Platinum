afe53ae36d7563743f666d73ac3bbe02ae7565ac
faisalbaqirt <faishalb0@gmail.com>
2023-10-31 17:23:47 +0700

    client only

--- server/db/db.js
+++
@@ -1,6 +0,0 @@
-const knex = require('knex');
-const knexfile = require('../knexfile');
-
-const db = knex(knexfile.development);
-
-module.exports = db;
--- server/middleware/isAuthenticated.js
+++
@@ -1,7 +0,0 @@
-const passport = require("../lib/passport");
-
-const isAuthenticated = passport.authenticate("jwt", {
-  session: false,
-});
-
-module.exports = isAuthenticated;
